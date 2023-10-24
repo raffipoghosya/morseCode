7c56099547ae2bafe1109b3bed2d8f406f34d33f
Imac <imac@Imacs-MacBook-Air.local>
2023-10-24 18:39:44 +0400

    add mor colors and new stylecss

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const morseCode = {
     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
 };
 
+let charCount = 0;
+
 function textToMorse(text) {
     let result = '';
     for (let c of text) {
@@ -24,4 +26,16 @@ function convertToMorse() {
     const inputText = document.getElementById('input').value;
     const morse = textToMorse(inputText);
     document.getElementById('output').innerText = `Morse code: ${morse}`;
+}
+
+function clearInput() {
+    document.getElementById('input').value = '';
+    document.getElementById('output').innerText = 'Morse code: ';
+    charCount = 0;
+    updateCharCount();
+}
+
+function updateCharCount() {
+    charCount = document.getElementById('input').value.length;
+    document.getElementById('charCount').innerText = `Characters entered: ${charCount}`;
 }
